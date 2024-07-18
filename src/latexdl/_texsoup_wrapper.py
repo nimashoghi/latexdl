ba7b8a75f06e12fa34ccc2c53cf478393dcbdb84
Nima Shoghi <nimashoghi@gmail.com>
2024-07-18 15:50:29 -0400

    URL fixes

--- src/latexdl/_texsoup_wrapper.py
+++ src/latexdl/_texsoup_wrapper.py
@@ -2,11 +2,16 @@ from typing import Any
 
 from TexSoup import TexSoup as _TexSoup
 
+from ._fix_urls import fix_latex_document
+
 
 def TexSoup(contents: str, *args: Any, **kwargs: Any):
     # For some reason, TexSoup struggles with parsing. We fix that here.
 
     # - $some math here$$some more math here$ (i.e., double dollar signs with no space in between)
     contents = contents.replace("$$", "$ $")
+
+    # Fix for URL handling
+    contents = fix_latex_document(contents)
 
     return _TexSoup(contents, *args, **kwargs)
